da29c7d74bb8828d1428653c2ff186530dc0754c
dev-grimnir <dev.grimnir@protonmail.com>
2026-05-07 15:12:24 -0400

    Bump version from 44.32 to 44.33

--- src/scripts/neonova-dashboard.user.js
+++ src/scripts/neonova-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NovaSubscriber - Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      44.32
+// @version      44.33
 // @description  Real-time customer modem connection dashboard (separate script)
 // @author       dev-grimnir
 // @match        https://admin.neonova.net/*
