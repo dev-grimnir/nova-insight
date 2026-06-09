bf9356f8bdf6f9e959e37075386308c685ac5469
dev-grimnir <dev.grimnir@protonmail.com>
2026-06-08 20:43:12 -0400

    Bump version number to 46.30

--- src/scripts/nova-dashboard.user.js
+++ src/scripts/nova-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NovaSubscriber - Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      46.29
+// @version      46.30
 // @description  Real-time customer modem connection dashboard (separate script)
 // @author       dev-grimnir
 // @match        https://admin.neonova.net/*
